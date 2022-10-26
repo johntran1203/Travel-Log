ac2e633b54706e1f7f281cc4190345202f6518de
johntran <tranjohn1211@gmail.com>
2022-10-25 22:00:41 -0700

    work on mapbox api create a marker and popup

--- src/pages/Destinations.js
+++ src/pages/Destinations.js
@@ -1,7 +1,35 @@
-const Destinations = () => {
-    return ( <h2>
-        Destinations will go here
-    </h2> );
+import * as React from 'react';
+import {render} from 'react-dom';
+import Map, {Marker, Popup} from 'react-map-gl';
+
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+const MAPBOX_TOKEN = 'pk.eyJ1Ijoiam9obnRyYW4xMjMiLCJhIjoiY2w5ZGl1NDl0MDZydTNubXdjYjlldHNrbyJ9.LTmjQwuRkC6QHByJiE1ozw'; // Set your mapbox token here
+
+function Destinations() {
+    const [showPopup, setShowPopup] = React.useState(true);
+    const [viewState, setViewState] = React.useState({
+        latitude: 33.81,
+        longitude: -117.91,
+        zoom: 10
+      });
+  return (
+    <Map
+      {...viewState}
+      onMove={evt => setViewState(evt.viewState)}
+      style={{width: 800, height: 600}}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+      mapboxAccessToken={MAPBOX_TOKEN}
+    >
+      <Marker longitude={-117.91} latitude={ 33.81} color="blue" />
+      {showPopup && (
+      <Popup longitude={-117.91} latitude={33.81}
+        anchor="bottom"
+        closeOnClick={() => setShowPopup(false)}>
+        You are here
+      </Popup>)}
+    </Map>
+  );
 }
- 
+
 export default Destinations;
