2f0a630a64817ea8d4e2644d9b5a7113da84ac46
johntran <tranjohn1211@gmail.com>
2022-10-26 16:24:40 -0700

    install material ui and connect the marker and popup together

--- src/pages/Destinations.js
+++ src/pages/Destinations.js
@@ -1,31 +1,43 @@
-import * as React from 'react';
-import {render} from 'react-dom';
-import Map, {Marker, Popup} from 'react-map-gl';
+import * as React from "react";
+import { render } from "react-dom";
+import Map, { Marker, Popup } from "react-map-gl";
+import RoomIcon from '@mui/icons-material/Room';
 
-import 'mapbox-gl/dist/mapbox-gl.css';
+import "mapbox-gl/dist/mapbox-gl.css";
 
-const MAPBOX_TOKEN = 'pk.eyJ1Ijoiam9obnRyYW4xMjMiLCJhIjoiY2w5ZGl1NDl0MDZydTNubXdjYjlldHNrbyJ9.LTmjQwuRkC6QHByJiE1ozw'; // Set your mapbox token here
+const MAPBOX_TOKEN =
+  "pk.eyJ1Ijoiam9obnRyYW4xMjMiLCJhIjoiY2w5ZGl1NDl0MDZydTNubXdjYjlldHNrbyJ9.LTmjQwuRkC6QHByJiE1ozw"; // Set your mapbox token here
 
 function Destinations() {
-    const [showPopup, setShowPopup] = React.useState(true);
-    const [viewState, setViewState] = React.useState({
-        latitude: 33.81,
-        longitude: -117.91,
-        zoom: 10
-      });
+  const [showPopup, setShowPopup] = React.useState(true);
+  console.log(showPopup);
+  const [viewState, setViewState] = React.useState({
+    latitude: 33.81,
+    longitude: -117.91,
+    zoom: 10,
+  });
+
   return (
     <Map
       {...viewState}
-      onMove={evt => setViewState(evt.viewState)}
-      style={{width: 800, height: 600}}
+      onMove={(evt) => setViewState(evt.viewState)}
+      style={{ width: 800, height: 600 }}
       mapStyle="mapbox://styles/mapbox/streets-v9"
       mapboxAccessToken={MAPBOX_TOKEN}
     >
-      <Marker longitude={-117.91} latitude={ 33.81} color="blue" />
-      {showPopup && (
+      <Marker longitude={-117.91} latitude={33.81}>
+        <RoomIcon onClick={e=> {
+            e.preventDefault()
+            setShowPopup(true)
+            console.log("hi")
+        }}/>
+      </Marker>
+            {showPopup && (
       <Popup longitude={-117.91} latitude={33.81}
         anchor="bottom"
-        closeOnClick={() => setShowPopup(false)}>
+        closeButton={true}
+        closeOnClick={false}
+        onClose={() => setShowPopup(false)}>
         You are here
       </Popup>)}
     </Map>
@@ -33,3 +45,5 @@ function Destinations() {
 }
 
 export default Destinations;
+
+<Marker longitude={-117.91} latitude={33.81} color="blue" />;
